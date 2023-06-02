08f1d38059f919983e85696165d9c3d2bb68d6b6
Jim Garcia <jim@cyberjungle.io>
2023-06-02 14:24:53 -0700

    update start.sh

--- start.sh
+++ start.sh
@@ -6,11 +6,11 @@ monitor_type=$(jq -r '.monitorType' "$config_file")
 
 # Check the monitorType value and run the appropriate script using nohup
 if [ "$monitor_type" = "solo" ]; then
-    nohup python3 solomonitor.py >/dev/null 2>&1 &
+    nohup python3 solomonitor.py &
 elif [ "$monitor_type" = "prbworker" ]; then
-    nohup python3 w.py >/dev/null 2>&1 &
+    nohup python3 workermonitor.py &
 elif [ "$monitor_type" = "prb" ]; then
-    nohup python3 prbmonitor.py >/dev/null 2>&1 &
+    nohup python3 prbmonitor.py &
 else
     echo "Invalid monitorType value in config.json"
     exit 1

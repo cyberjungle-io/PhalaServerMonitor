7f0370691b33b227aac5260baa887e9191a48bf6
Jim Garcia <jim@cyberjungle.io>
2023-06-08 14:31:02 -0700

    workermonitor

---
+++ ExecCmdWorker.py
@@ -0,0 +1,26 @@
+
+import subprocess
+
+def SendPhalaUpdate():
+    s = subprocess.call("docker-compose -f /home/oak/worker/docker-compose.yml down", shell = True)
+    s = subprocess.call("docker-compose -f /home/oak/worker/docker-compose.yml pull", shell = True)
+    s = subprocess.call("docker-compose -f /home/oak/worker/docker-compose.yml up -d", shell = True)
+    
+
+def SendPhalaRestart():
+    s = subprocess.call("docker-compose -f /home/oak/worker/docker-compose.yml down", shell = True)
+    s = subprocess.call("docker-compose -f /home/oak/worker/docker-compose.yml up -d", shell = True)
+    
+
+
+def SendPhalaStart():
+    s = subprocess.call("docker-compose -f /home/oak/worker/docker-compose.yml up -d", shell = True)
+
+def SendPhalaStop():
+    s = subprocess.call("docker-compose -f /home/oak/worker/docker-compose.yml down", shell = True)
+
+
+
+
+
+SendPhalaStop()
--- workermonitor.py
+++ workermonitor.py
@@ -5,7 +5,7 @@ import PhalaMonitor
 import PhalaBlockChain
 import time
 import math
-import ExecCmd
+import ExecCmdWorker
 
 
 with open("config.json") as file:
@@ -45,23 +45,54 @@ while True:
     headers = {'Content-type': 'application/json','monitor_key':monitorKey} 
     try:
         r = requests.post(url, data=data_json, headers=headers,timeout=30)
-        result = r.json()
-        print(result)
+        rest_result = r.json()
+        print(rest_result)
        # khala = r.json()["result"]
-        if result["send_update_command"]:
-            ExecCmd.SendPhalaUpdate()
+        for cmd in rest_result["commands"]:
+            print(cmd["command"])
+            logdata = {
+                 "command": cmd["command"],
+                 "monitorType": monitorType,
+                 "serviceName": serviceName,
+                 "org_id" : monitorKey,
+                 "timestamp": math.trunc(time.time()*1000)
+            }
+            if cmd["command"] == "update phala":
+                ExecCmdWorker.SendPhalaUpdate()
 
-        if result["send_restart_command"]:
-            ExecCmd.SendPhalaRestart()
+            if cmd["command"] == "restart phala":
+                ExecCmdWorker.SendPhalaRestart()
+                
 
-        if result["send_stop_command"]:
-                ExecCmd.SendPhalaStop()
+            if cmd["command"] == "autorestart phala":
+                
+                logdata["phalaStatus"] = result
+                tlogs = {
+                         "phala-pruntime":LinuxMonitor.get_docker_logs('~/solo-mining-scripts-main/docker-compose.yml',"phala-pruntime",100),
+                         }
+                logdata["dockerLogs"] = tlogs
+                ExecCmdWorker.SendPhalaRestart()
 
-        if result["send_start_command"]:
-                ExecCmd.SendPhalaStart()
+            if cmd["command"] == "stop phala":
+                ExecCmdWorker.SendPhalaStop()
 
-        if result["send_reboot"]:
-                print("send_reboot")
+            if cmd["command"] == "start phala":
+                ExecCmdWorker.SendPhalaStart()
+
+            
+            if cmd["command"] == "phala logs":
+                logdata["phalaStatus"] = result
+                tlogs = {
+                        "phala-pruntime":LinuxMonitor.get_docker_logs('~/solo-mining-scripts-main/docker-compose.yml',"phala-pruntime",100),
+                }
+                logdata["dockerLogs"] = tlogs
+
+            
+            data_json = json.dumps(logdata)
+            print(data_json)
+            url = monitorUrl + "/worker/saveWorkerLog"
+            rc = requests.post(url, data=data_json, headers=headers,timeout=30)
+     
        
     except:
         khala = {}
